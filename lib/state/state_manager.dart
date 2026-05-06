cd720ba150836e8097471ea4720dd145d99d65e3
Wang-satalo <Wongaiyara.Suriya@gmail.com>
2026-05-06 16:27:37 +0700

    Remove unused import of 'package:flutter/widgets.dart' from state_manager.dart

--- lib/state/state_manager.dart
+++ lib/state/state_manager.dart
@@ -1,5 +1,4 @@
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reader_app/model/chapters.dart';
 import 'package:reader_app/model/comic.dart';
